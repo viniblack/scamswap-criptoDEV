85fe306d86b3afedf6b721350cb74e5de92c2f38
Douglas Melo <douglasportmelo@gmail.com>
2022-06-16 07:03:32 -0300

    Contrato Vending Machine quase finalizado e já interagindo com o contrato do token!

---
+++ contracts/Token.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+
+    function totalSupply() external view returns(uint256);
+    function balanceOf(address account) external view returns(uint256);
+    function transfer(address recipient, uint256 amount) external returns(bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+
+    //Implementado
+    event Transfer(address from, address to, uint256 value);
+
+    //Não está implementado (ainda)
+    //event Approval(address owner, address spender, uint256 value);
+
+}
+
+contract ScamCoin is IERC20 {
+
+     // Enum
+    enum Status { PAUSED, ACTIVE, CANCELLED }
+
+    //Properties
+    address private owner;
+    string public constant name = "ScamCoin";
+    string public constant symbol = "SCN";
+    uint8 public constant decimals = 18;  //Default dos exemplos é sempre 18
+    uint256 private totalsupply;
+    Status contractState;
+    uint256 valorToken;
+
+    mapping(address => uint256) private addressToBalance;
+
+    // Modifiers
+    modifier isOwner() {
+        require(msg.sender == owner , "Sender is not owner!");
+        _;
+    }
+
+    modifier isActive() {
+        require(contractState == Status.ACTIVE, "Contract is not Active!");
+        _;
+    }
+
+    // Events
+    event Burn(address owner, uint256 value, uint256 supply);
+    event Mint(address owner, uint256 BalanceOwner, uint256 amount, uint256 supply);
+
+ 
+    //Constructor
+    constructor(uint256 total) {
+        owner = msg.sender;
+        totalsupply = total;
+        addressToBalance[msg.sender] = totalsupply;
+        contractState = Status.ACTIVE;
+    }
+
+    //Public Functions
+    function totalSupply() public override view returns(uint256) {
+        return totalsupply;
+    }
+
+    function balanceOf(address tokenOwner) public override view returns(uint256) {
+        return addressToBalance[tokenOwner];
+    }
+
+    function transfer(address receiver, uint256 quantity) public isActive override returns(bool) {
+        require(quantity <= addressToBalance[msg.sender], "Insufficient Balance to Transfer");
+        addressToBalance[msg.sender] -= quantity;
+        addressToBalance[receiver] += quantity;
+        emit Transfer(msg.sender, receiver, quantity);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint amount)public override returns(bool) { 
+        //allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(tokens);
+        //require(amount <= balances[owner]);
+        //require(amount <= allowed[owner][msg.sender]);
+        //allowed[owner][msg.sender] = allowed[owner][msg.sender]-numTokens;
+        addressToBalance[from] -= amount;
+        addressToBalance[to] += amount;
+        //Transfer(_from, _to, tokens);
+        return true;
+    }
+
+    function state() public view returns(Status) {
+        return contractState;
+    }
+
+    function setState(uint8 status) public isOwner {
+        require(status == 1 || status == 0, "Invalid status");
+        if(status == 1){
+            contractState = Status.ACTIVE;
+        }else {
+            contractState = Status.PAUSED;
+        }
+   
+    }
+
+    function mint(uint256 amount) public isActive isOwner {
+
+        require(amount > 0, "Invalid mint value.");
+        
+        totalsupply += amount;
+        addressToBalance[owner] += amount;
+        
+        emit Mint(owner,addressToBalance[owner], amount, totalSupply());       
+    }
+
+
+    function burn(uint256 amount) public isActive isOwner {
+        require(amount > 0, "Invalid burn value.");
+        require(totalSupply() >= amount, "The amount exceeds your balance.");
+        totalsupply -= amount;
+        addressToBalance[owner] -= amount;
+
+        emit Burn(owner, amount, totalSupply());
+    }
+
+    // Kill
+    function kill() public isOwner {
+        contractState = Status.CANCELLED;
+        selfdestruct(payable(owner));
+    } 
+}
--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -1,57 +1,116 @@
-//SPDX-License-Identifier: MIT
+import "./Token.sol";
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.0;
 
 contract VendingMachine{
-    address public owner;
-    mapping(address => uint) public tokensBalances;
-    mapping(address => uint) public ethersBalances;
-    uint256 private salesPrice =  0.8 ether;
-    uint256 private purchasePrice = 1 ether;  
+    address payable public admin;
+    uint256 private salesPrice =  1 ether;
+    uint256 private purchasePrice = 2 ether; 
+    address public tokenAddress;
 
-    constructor(){
-        owner = msg.sender;
-        tokensBalances[address(this)] = 100;
-        
+    constructor(address token){
+        admin = payable(msg.sender);
+        tokenAddress = token;
     }
+
+    event Received(address, uint);
 
     modifier isAdmin() {
-        require(msg.sender == owner , "Sender is not admin!");
+        require(msg.sender == admin , "Sender is not admin!");
         _;
     }
 
-    /*modifier isZero() {
+    function getBalanceTokens() public view returns (uint256) {
+        return ScamCoin(tokenAddress).balanceOf(address(this));
+    }
+
+    function getBalanceTokensForAddress(address _address) public view returns (uint256) {
+        return ScamCoin(tokenAddress).balanceOf(_address);
+    }
+
+    function getBalanceEthers() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+     function getSalesPrice() public view returns (uint256) {
+        return salesPrice;
+    }
+
+     function getPurchasePrice() public view returns (uint256) {
+        return purchasePrice;
+    }
+
+    function getBanlanceAddress(address _address) public view returns (uint256) {
+        return address(_address).balance ;
+    }
+    
+    function setSalesPrice(uint256 _salesPrice) public isAdmin{
+        salesPrice = _salesPrice * 1 ether;
+    }
+
+    function setPurchasePrice(uint256 _purchasePrice ) public isAdmin{
+        purchasePrice  = _purchasePrice * 1 ether;
+    }
+    
+    function restockEthers() public isAdmin payable{
         require(msg.value > 0 , "The value entered must not be zero!");
-        _;
-    }*/
-
-    function getBalance() public view returns (uint) {
-        return tokensBalances[address(this)];
+        //Não seria importante verificar se os usuários não enviam mais dinheiro do que têm porque o protocolo trata disso.
+        //https://ethereum-stackexchange-com.translate.goog/questions/91416/best-way-to-check-balance-msg-sender-balance-vs-balancesmsg-sender?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc
+        //chamada ao balance não funciona, retorna valor errado - require(getBanlanceAddress(msg.sender) > 0 , "Not Balance!");
+        //chamada ao balance não funciona, retorna valor errado - require(getBanlanceAddress(msg.sender)  >= msg.value, "Valor na carteira menor do que o valor enviado!");
+        //todo colocar evento
     }
-    
-    function newSalesPrive(uint256 _salesPrice) public isAdmin payable {
-        salesPrice = _salesPrice * 0.1 ether;
-    }
-
-    function newPurchasePrice(uint256 _purchasePrice ) public isAdmin payable {
-        purchasePrice  = _purchasePrice * 0.1 ether;
-    }
-    
-    function restockEthers(uint amount) public isAdmin payable{
-        require(amount > 0 , "The value entered must not be zero!");
-        ethersBalances[address(this)]+= amount;
+   
+    function restockTokens(uint256 amountTokens) public isAdmin{
+        require(amountTokens > 0 , "The value entered must not be zero!"); 
+        require(getBalanceTokensForAddress(msg.sender) >= amountTokens , "Nao tem tokens suficiente!");  
+        require(ScamCoin(tokenAddress).transferFrom(msg.sender, address(this), amountTokens), "Tranferencia de tokens falhou!");
     }
 
-     function restockTokens(uint256 amount) public isAdmin{
-        require(amount > 0 , "The value entered must not be zero!");  
-        tokensBalances[address(this)]+= amount;
+    function sales(uint256 amountTokens) public payable{
+        require(amountTokens > 0 , "The value entered must not be zero!");
+        require(getBalanceTokensForAddress(msg.sender) > 0, "User wallet has no tokens!");
+        require(getBalanceTokensForAddress(msg.sender) >= amountTokens, "There are not enough tokens in the wallet for the sale!");
+        require(address(this).balance >= amountTokens * salesPrice, "Insuficiente saldo de ethers na Vending Machine");     
+        require(ScamCoin(tokenAddress).transferFrom(msg.sender, address(this), amountTokens), "Tranferencia de tokens falhou!");
+        payable(msg.sender).transfer(amountTokens * salesPrice);
+        //todo colocar evento
+    } 
+       
+    function purchase(uint256 amountTokens) public payable{
+        require(amountTokens > 0 , "The value entered must not be zero!");
+        require(getBalanceTokensForAddress(address(this)) >= amountTokens, "Insuficiente quantidade de tokens na Vendi Machine para a compra!");
+        //chamada ao balance não funciona, retorna valor errado - require(address(msg.sender).balance >= amountTokens * purchasePrice, "Insuficiente quantidade de ethers na sua carteira para a compra!");
+        require(msg.value >= amountTokens * purchasePrice, "Valor enviado insuficiente para a compra");
+        require(ScamCoin(tokenAddress).transferFrom(address(this), msg.sender, amountTokens), "Tranferencia de tokens falhou!");
+        //Devolve o troco se o usuário, se enviou um valor maior do que deveria seer pago!
+        if(msg.value > (amountTokens * purchasePrice)){
+           uint256  payback = msg.value - (amountTokens * purchasePrice);
+           payable(msg.sender).transfer(payback);
+        }    
+         //todo colocar evento
+    } 
+
+    function withdrawEthers(address addressReceive, uint256 amountEthers) public isAdmin payable{
+        require(amountEthers > 0 , "O valor de Ethers para sacar nao pode ser zero!");
+        require(address(this).balance > 0, "A carteira da Vending Machine nao possui Ethers!");
+        require(address(this).balance >= amountEthers * 1 ether, "A carteira da Vending Machine nao possui a quantidade de Ethers solicitados para o saque!");
+        payable(addressReceive).transfer(amountEthers * 1 ether);
     }
 
-    function purchase(uint256 amount) public payable{
-        require(amount > 0 , "The value entered must not be zero!");
-        require(tokensBalances[address(this)] >= amount, "Not enough tokens for this value!");
-        ethersBalances[address(this)] += amount * 1 ether;
-        tokensBalances[address(this)] -= amount;
-        tokensBalances[address(msg.sender)] += amount;
+    function withdrawAllEthers() public isAdmin payable{
+        require(address(this).balance > 0, "A carteira da Vending Machine nao possui Ethers!");
+        payable(msg.sender).transfer(address(this).balance);
     }
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+
+    //todo - fazer função kill
 }
+
+
+
+
